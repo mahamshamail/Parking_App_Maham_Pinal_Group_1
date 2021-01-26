4098a3e3ec5e4710a209ad86ace838aec1420409
maham <mahamshamail@gmail.com>
2021-01-25 22:20:38 -0500

    new data and permissions

---
+++ Parking_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parking_App
+//
+//  Created by Maham Shamail on 25/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
